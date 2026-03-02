708e827fac290c30ee361e9b2804937b65c5b114
Brendan-HunterOfBugs <brendanhunter02@gmail.com>
2026-03-01 20:12:41 -0800

    Add input prompt for goods total, payment, and discount code to find change to give back to the user;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,66 @@
-// eslint-disable-next-line no-unused-vars
-/* global output, input */
-// eslint-disable-next-line no-unused-vars
+
 async function main() {
-  // This is where the code you're actually experimenting with goes.
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+  //user enter total cost of goods;
+  let totalCost = parseFloat(await input("Enter the total cost of goods (we have cameras!): $"));
+  while (isNaN(totalCost) || totalCost < 0) {
+    output("Invalid amount! Please try again.")
+    totalCost = parseFloat(await input("Enter the total cost of goods (we have cameras!): $"));
+    }
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+
+  //user enters payment amount;
+  let cashReceived = parseFloat(await input("Enter your payment: $"));
+  while (isNaN(cashReceived) || cashReceived < 0) {
+      output("Invalid amount! Please try again.");
+      cashReceived = parseFloat(await input("Enter your payment: $"));
+    }
+
+
+  //user enters discount code;
+  let discountInput = parseInt(await input("Enter a discount code: "));
+  let discountOff = 0
+
+  switch (discountInput) {
+    case 0:
+      output(`Sorry, no discount was applied.`)
+      break;
+    case 1:
+      discountOff = 0.10;
+      output("You received 10% off in savings today!")
+      break;
+    case 2:
+      discountOff = 0.15;
+      output("You received 15% off in savings today!")
+      break;
+    case 3:
+      discountOff = 0.25;
+      output("You received 25% off in savings today!")
+      break;
+    case 4:
+      discountOff = 0.35;
+      output("You received 35% off in savings today!")
+      break;
+    case 5:
+      discountOff = 0.40;
+      output("You received 40% off in savings today!")
+      break;
+    default:
+      output("Invalid code. No discount applied.")
+      break;
   }
+
+
+  
+  //calculate change to give back to user with discount;
+  let finalTotal = totalCost - (totalCost * discountOff)
+  let changeBack = cashReceived - finalTotal
+  
+  if (changeBack >= 0) {
+    output(`Your change is $${changeBack.toFixed(2)}!`);
+  }
+  else {
+    output("Insufficient Payment! Please try again.")
+  }
+
 }
